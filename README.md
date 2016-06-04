7336326d23a4e1e40903b4b315412d7987f66472
Stan Janssen <stanjanssen@outlook.com>
2016-06-05 00:28:41 +0200

    PyRegions basics

---
+++ README.md
@@ -0,0 +1,17 @@
+# PyRegions
+PyRegions is a simple Sublime Text plugin to add custom toggable regions in Python files. 
+Other plugins (such as SyntaxFold) do a great, if not better, job at this. However, I've always wanted to create a Sublime Text plugin.
+
+## Features
+* Toggle regions independent of indentation.
+* Shows the name of the region your cursor is currently in in the status bar
+
+## Usage
+Just add the following two lines around the block you want to create a region of:
+
+    #region:<name>
+    #endregion:<name>
+
+## Development ideas
+* Ability to use different comment syntax
+* Perhaps be less strict on adding the name to the end of the region
---
+++ pyregions.py
@@ -0,0 +1,59 @@
+import sublime, sublime_plugin
+import re
+
+def get_view_text(view):
+    return view.substr(sublime.Region(0, view.size()))
+
+def get_cursor(view):
+    return [region.begin() for region in view.sel()]
+
+PLUGIN_NAME = "PyRegions"
+names = []
+class PyRegionsMarkCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        global names
+        names = []
+        self.view.erase_regions(PLUGIN_NAME)
+        regions = []
+        contents = get_view_text(self.view)
+        matches = re.finditer(r'#region:(.*?)$(.|[\n])*?#endregion:\1', contents, re.M)
+        for match in matches:
+            region = sublime.Region(match.start(), match.end())
+            names.append(match.group(1))
+            lines = self.view.lines(region)
+            region = sublime.Region(lines[0].b, lines[-1].b)
+            regions.append(region)
+        self.view.add_regions(PLUGIN_NAME, regions, "comment", "bookmark", sublime.HIDDEN)
+
+class PyRegionsToggleCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command('py_regions_mark')
+        cursors = get_cursor(self.view)
+        for cursor in cursors:
+            for region in self.view.get_regions(PLUGIN_NAME):
+                lines = self.view.lines(region)
+                start, end = lines[0].a, lines[-1].b
+                if start <= cursor <= end:
+                    if not self.view.fold(region):
+                        self.view.unfold(region)
+                    break
+
+class PyRegionsListener(sublime_plugin.EventListener):
+    def on_selection_modified(self, view):
+        status_set = False
+        cursors = get_cursor(view)
+        for cursor in cursors:
+            for index, region in enumerate(view.get_regions(PLUGIN_NAME)):
+                lines = view.lines(region)
+                start, end = lines[0].a, lines[-1].b
+                if start <= cursor <= end:
+                    view.set_status(PLUGIN_NAME, "Region: " + names[index])
+                    status_set = True
+        if not status_set:
+            view.erase_status(PLUGIN_NAME)
+
+    def on_load(self, view):
+        view.run_command('py_regions_mark')
+
+    def on_pre_save(self, view):
+        view.run_command('py_regions_mark')
